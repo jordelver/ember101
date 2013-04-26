50b47eb2c2fd46df8ff53c9c112098278524d84d
Jordan Elver <jordan.elver@gmail.com>
2013-04-26 12:27:58 +0100

    Update the amount of seconds on the page

--- 01-intro-to-ember/app.js
+++ 01-intro-to-ember/app.js
@@ -1,3 +1,8 @@
 var App = Ember.Application.create();
 App.name = "Hello, world!";
+App.secondsViewed = 0;
+
+setInterval(function() {
+  App.set('secondsViewed', App.get('secondsViewed') + 1);
+}, 1000);
 
