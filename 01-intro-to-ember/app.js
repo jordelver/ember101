6f5cfca136bb8e1aac8d7b1a92d1311a47cb5375
Jordan Elver <jordan.elver@gmail.com>
2013-04-26 12:32:06 +0100

    Set attributes on application creation

--- 01-intro-to-ember/app.js
+++ 01-intro-to-ember/app.js
@@ -1,6 +1,7 @@
-var App = Ember.Application.create();
-App.name = "Hello, world!";
-App.secondsViewed = 0;
+var App = Ember.Application.create({
+  name: "Hello, world!",
+  secondsViewed: 0
+});
 
 setInterval(function() {
   App.set('secondsViewed', App.get('secondsViewed') + 1);
