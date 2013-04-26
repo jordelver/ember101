ad07cf4429de495dcc974f74fb0dcdb4a9dfba7e
Jordan Elver <jordan.elver@gmail.com>
2013-04-26 21:35:12 +0100

    Add timer back onto page
    'activate' and 'deactivate' are triggered on entering the route and leaving
    the route respectively.

--- 02-your-first-route/app.js
+++ 02-your-first-route/app.js
@@ -2,7 +2,18 @@ var App = Ember.Application.create();
 
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
-    return { name: 'My App' }
+    return { name: 'My App', timer: 0 }
+  },
+
+  activate: function() {
+    this.interval = setInterval(function() {
+      var timer = this.get('controller.model.timer');
+      this.set('controller.model.timer', timer + 1);
+    }.bind(this), 1000);
+  },
+
+  deactivate: function() {
+    clearInterval(this.interval);
   }
 });
 
