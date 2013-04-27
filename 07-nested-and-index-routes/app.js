f2456aca1c56774deac1573743247a841783831f
Jordan Elver <jordan.elver@gmail.com>
2013-04-27 09:43:27 +0100

    Add nested routes and templates

--- 07-nested-and-index-routes/app.js
+++ 07-nested-and-index-routes/app.js
@@ -1,7 +1,10 @@
 var App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource('about');
+  this.resource('about', function() {
+    this.route('location');
+    this.route('product');
+  });
   this.resource('login');
 });
 
