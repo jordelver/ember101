0bc0c4626e0a81df5e9a5ee1bf3bf80803305bb7
Jordan Elver <jordan.elver@gmail.com>
2013-04-27 08:14:41 +0100

    Setup basis for edit form

---
+++ 05-two-way-event-binding/app.js
@@ -0,0 +1,39 @@
+var App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('user', { path: '/users/:user_id' });
+});
+
+App.UserRoute = Ember.Route.extend({
+  model: function(params) {
+    return users[params.user_id];
+  }
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return users;
+  }
+});
+
+var users = [
+  {
+    id: 1,
+    first: 'Ryan',
+    last: 'Florence',
+    avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
+  },
+  {
+    id: 2,
+    first: 'Tom',
+    last: 'Dale',
+    avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
+  },
+  {
+    id: 3,
+    first: 'Yehuda',
+    last: 'Katz',
+    avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
+  }
+];
+
